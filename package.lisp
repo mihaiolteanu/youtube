e830f93a56956fb26b0fe1cfbd823df4082083f9
mihaiolteanu <mihai_olteanu@fastmail.fm>
2019-06-10 15:53:24 +0300

    Update package

--- package.lisp
+++ package.lisp
@@ -1,12 +1,17 @@
 (defpackage :youtube
   (:use :cl)
   (:import-from :uiop :run-program)
+  (:import-from :uiop :launch-program)
   (:import-from :alexandria :if-let)
   (:import-from :yason :parse)
+  (:import-from :cl-ppcre :scan-to-strings)
   (:export play
            play/pause
            replay
            seek
            percent-pos
+           time-pos
+           duration
            switch-to-browser
+           turn-video-on
            quit))
